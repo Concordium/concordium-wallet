d0c69fe2192d428d87f074b11c61a46a3065bf78
Hjort <shj@concordium.com>
2023-10-23 15:28:13 +0200

    Add a widget library, widget book

---
+++ bin/widgetbook.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:widgetbook/widgetbook.dart';
+import 'package:concordium_wallet/shared_components/button.dart';
+import 'package:concordium_wallet/shared_components/button_material.dart';
+
+void main() {
+  runApp(const WidgetbookApp());
+}
+
+class WidgetbookApp extends StatelessWidget {
+  const WidgetbookApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Widgetbook.material(
+      addons: [
+        AlignmentAddon(),
+      ],
+      directories: [__button(), __buttonMaterial()],
+    );
+  }
+
+  WidgetbookComponent __button() {
+    return WidgetbookComponent(
+      name: '$Button',
+      useCases: [
+        WidgetbookUseCase(
+          name: 'Default',
+          builder: (context) {
+            return Button(
+              decoration: decoration,
+              text: context.knobs.string(
+                label: 'Text',
+                initialValue: 'Hello World!',
+              ),
+            );
+          },
+        )
+      ],
+    );
+  }
+
+  WidgetbookComponent __buttonMaterial() {
+    return WidgetbookComponent(
+      name: '$ButtonMaterial',
+      useCases: [
+        WidgetbookUseCase(
+          name: 'Default',
+          builder: (context) {
+            return ButtonMaterial(
+              decoration: decoration,
+              text: context.knobs.string(
+                label: 'Text',
+                initialValue: 'Hello World!',
+              ),
+            );
+          },
+        )
+      ],
+    );
+  }
+}
