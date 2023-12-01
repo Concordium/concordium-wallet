98af20bc42a118abd39e235d71739ff8572f15a0
Hjort <shj@concordium.com>
2023-12-01 14:07:37 +0100

    Fix localization

--- test/helpers.dart
+++ test/helpers.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 /// Wrap MaterialApp and Scaffold around the given widget.
-Widget wrapMaterial({required Widget? child}) => MaterialApp(home: Scaffold(body: child));
+Widget wrapMaterial({required Widget? child}) => MaterialApp(
+    localizationsDelegates: AppLocalizations.localizationsDelegates,
+    supportedLocales: AppLocalizations.supportedLocales,
+    home: Scaffold(body: child));
