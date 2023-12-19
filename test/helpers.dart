bb0b093d12b20334786dd9a7d2f557c4f2435fa1
Rene Hansen <rh@concordium.com>
2023-12-19 04:14:47 +0100

    Add theme extension for widget tests

--- test/helpers.dart
+++ test/helpers.dart
@@ -1,4 +1,18 @@
+import 'package:concordium_wallet/design_system/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 /// Wrap MaterialApp and Scaffold around the given widget.
-Widget wrapMaterial({required Widget? child}) => MaterialApp(home: Scaffold(body: child));
+/// The theme is also registered in order to be able to make use of the CcdTheme
+Widget wrapMaterial({required Widget? child}) =>
+    MultiBlocProvider(
+      providers: [
+        BlocProvider<AppTheme>(create: (_) => AppTheme()),
+      ],
+      child: Builder(
+        builder: (context) => MaterialApp(
+          home: Scaffold(body: child),
+          theme: context.watch<AppTheme>().state.themeData,
+        ),
+      ),
+    );
