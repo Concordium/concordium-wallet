08d19fd0c140564e33b5e7af57f579e5bc96d6d1
Rene Hansen <rh@concordium.com>
2023-12-20 13:39:23 +0100

    Group widgetbook colors in folder

--- lib/widgetbook/foundation/colors/colors_folder.dart
+++ lib/widgetbook/foundation/colors/colors_folder.dart
@@ -1,13 +1,11 @@
 import 'package:concordium_wallet/widgetbook/foundation/colors/view_semantic_colors.dart';
 import 'package:concordium_wallet/widgetbook/foundation/colors/view_internal_colors.dart';
-import 'package:concordium_wallet/widgetbook/foundation/typography/view_typography.dart';
-import 'package:concordium_wallet/widgetbook/graphics/icons/view_icons.dart';
 import 'package:concordium_wallet/widgetbook/helpers/default_folder.dart';
 
 class ColorsFolder extends DefaultFolder {
   ColorsFolder()
       : super(name: 'Colors', children: [
-    ViewSemanticColors().component,
-    ViewInternalColors().component,
-  ]);
+          ViewSemanticColors().component,
+          ViewInternalColors().component,
+        ]);
 }
--- lib/widgetbook/foundation/foundation_folder.dart
+++ lib/widgetbook/foundation/foundation_folder.dart
@@ -1,6 +1,4 @@
 import 'package:concordium_wallet/widgetbook/foundation/colors/colors_folder.dart';
-import 'package:concordium_wallet/widgetbook/foundation/colors/view_semantic_colors.dart';
-import 'package:concordium_wallet/widgetbook/foundation/colors/view_internal_colors.dart';
 import 'package:concordium_wallet/widgetbook/foundation/typography/view_typography.dart';
 import 'package:concordium_wallet/widgetbook/graphics/icons/view_icons.dart';
 import 'package:concordium_wallet/widgetbook/helpers/default_folder.dart';
