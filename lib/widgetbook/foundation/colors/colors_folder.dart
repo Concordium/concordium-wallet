6bc647d7c74898132f0683f44ef9c450bc0bb181
Rene Hansen <rh@concordium.com>
2023-12-20 13:38:44 +0100

    Group widgetbook colors in folder

--- lib/widgetbook/foundation/colors/colors_folder.dart
+++ lib/widgetbook/foundation/colors/colors_folder.dart
@@ -1,15 +1,13 @@
-import 'package:concordium_wallet/widgetbook/foundation/colors/colors_folder.dart';
 import 'package:concordium_wallet/widgetbook/foundation/colors/view_semantic_colors.dart';
 import 'package:concordium_wallet/widgetbook/foundation/colors/view_internal_colors.dart';
 import 'package:concordium_wallet/widgetbook/foundation/typography/view_typography.dart';
 import 'package:concordium_wallet/widgetbook/graphics/icons/view_icons.dart';
 import 'package:concordium_wallet/widgetbook/helpers/default_folder.dart';
 
-class FoundationFolder extends DefaultFolder {
-  FoundationFolder()
-      : super(name: 'Foundation', children: [
-          ColorsFolder(),
-          ViewTypography().component,
-          ViewIcons().component,
-        ]);
+class ColorsFolder extends DefaultFolder {
+  ColorsFolder()
+      : super(name: 'Colors', children: [
+    ViewSemanticColors().component,
+    ViewInternalColors().component,
+  ]);
 }
--- lib/widgetbook/foundation/colors/view_semantic_colors.dart
+++ lib/widgetbook/foundation/colors/view_semantic_colors.dart
@@ -3,8 +3,8 @@ import 'package:concordium_wallet/widgetbook/helpers/ccd_widgetbook_theme.dart';
 import 'package:concordium_wallet/widgetbook/helpers/default_component.dart';
 import 'package:flutter/material.dart';
 
-class ViewColors extends DefaultComponent with ColorBuilderHelper {
-  ViewColors() : super(name: 'Colors');
+class ViewSemanticColors extends DefaultComponent with ColorBuilderHelper {
+  ViewSemanticColors() : super(name: 'Semantic colors');
 
   @override
   Widget buildDefault(BuildContext context) {
